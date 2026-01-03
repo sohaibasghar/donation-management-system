5ee70c7e63d82d6a0c9eef9e6b68cae664f3d464
Sohaib Asghar <sohaib@mavrictech.com>
2026-01-03 14:47:30 +0500

    cache fix

--- src/lib/providers/query-provider.tsx
+++ src/lib/providers/query-provider.tsx
@@ -9,7 +9,7 @@ export function QueryProvider({ children }: { children: React.ReactNode }) {
       new QueryClient({
         defaultOptions: {
           queries: {
-            staleTime: 60 * 1000, // 1 minute
+            staleTime: 0,
             refetchOnWindowFocus: false,
           },
         },
